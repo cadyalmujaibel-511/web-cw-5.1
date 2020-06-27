a7f0c72b6e66ba65e1d441e92c7492c281b21427
511- كادي أنس المجيبل <66686892+cadyalmujaibel-511@users.noreply.github.com>
2020-06-27 21:53:02 +0300

    cw5.1 done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,46 @@
 // السؤال الأول
-// 1
-
+// 1 
+const name = "كادي";
 // 2
-
+let age = 16 ;
 // 3 and 4
-
+console.log("اسمي" +" "+ name +" "+"وعمري"+ age) ;
 // 5 and 6
-
+age = age*5;
+console.log(age)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies = ["ركوب الدراجة","البرمجة","الغناء"]
 // 2
-
+hobbies.push(16)
+hobbies[4] = 16
+console.log(hobbies)
+console.log(hobbies.length)
 // 3
-
+hobbies.pop()
+console.log(hobbies)
 // 4
-
+let student = {
+    track: "مسار الويب",
+    language: "JavaScript"
+}
 // 5
-
+student["TAs"] = ["نانسي","موضي","حسين","رهف"]
+console.log(student)
 // 6
-
+console.log(student["track"])
+console.log(student.track)
 // 7
-
+console.log(student["TAs"][0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
-
+student["sayHello"] = function() {console.log("مرحبًا")}
+console.log(student)
 // 2
-
+student["sayHello"]()
+student["sayHello"]()
 // console results => console.png
